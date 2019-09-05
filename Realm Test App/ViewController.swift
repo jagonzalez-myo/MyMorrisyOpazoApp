37cf9051b72b7ff6461996d91fb300819a6f4c08
Javier Gonzalez <jagonzalez@livly.io>
2019-09-05 18:11:18 -0400

    Initial Commit

---
+++ Realm Test App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm Test App
+//
+//  Created by Javier González Ovalle on 9/5/19.
+//  Copyright © 2019 Javier González Ovalle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Realm Test AppTests/Realm_Test_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Realm_Test_AppTests.swift
+//  Realm Test AppTests
+//
+//  Created by Javier González Ovalle on 9/5/19.
+//  Copyright © 2019 Javier González Ovalle. All rights reserved.
+//
+
+import XCTest
+@testable import Realm_Test_App
+
+class Realm_Test_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
