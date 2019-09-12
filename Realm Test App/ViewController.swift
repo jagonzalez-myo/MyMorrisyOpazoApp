a25589771142660ff1b7f229aa6ca1e62c9835a4
Javier Gonzalez <jagonzalez@livly.io>
2019-09-12 15:26:32 -0300

    Adds table view

--- Realm Test App/ViewController.swift
+++ Realm Test App/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("hello world")
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
