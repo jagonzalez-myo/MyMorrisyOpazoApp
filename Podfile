7530194334cc820a2991c03cdcde7f69e14d2609
Javier Gonzalez <jagonzalez@livly.io>
2019-11-05 18:17:00 -0300

    simple table view.

--- Podfile
+++ Podfile
@@ -7,16 +7,5 @@ target 'Realm Test App' do
 
   # Pods for Realm Test App
   pod 'R.swift'
-  pod 'RealmSwift'
-
-  target 'Realm Test AppTests' do
-    inherit! :search_paths
-    # Pods for testing
-  end
-
-  target 'Realm Test AppUITests' do
-    inherit! :search_paths
-    # Pods for testing
-  end
-
+  ## pod 'RealmSwift'
 end
--- Realm Test App/ViewController.swift
+++ Realm Test App/ViewController.swift
@@ -8,14 +8,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let note = Note()
-        note.id = 1
-        note.title = "Shopping List"
-        note.text = "milk, butter, cake, apples"
-        print("hello world")
-        
     }
     
     @IBAction func enterButtonAction(_ sender: UIButton) {
